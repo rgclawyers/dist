ae57c7cfd440cc9eb3f9296213a45bad532f3b51
Ishaan Goel <ishaangoel.99@gmail.com>
2024-03-26 19:16:04 +0530

    udpate

--- assets/OurTeamView.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/TeamMembers.js","assets/index.js","assets/index.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{d as e,m as a,w as s,o,f as t,a as r,e as n,u as i,y as l,D as m,E as c}from"./index.js";import{_ as u}from"./BodyTemplate.vue_vue_type_script_setup_true_lang.js";import{L as _}from"./LoadingAnimation.js";const d=r("img",{src:l,alt:"Image",class:"object-center shadow-inner lg:min-h-screen lg:object-cover"},null,-1),p=r("br",null,null,-1),g={id:"bg",class:"bg-white"},b=e({__name:"OurTeamView",setup(e){const l=m({loader:()=>c((()=>import("./TeamMembers.js")),__vite__mapDeps([0,1,2])),loadingComponent:_});return(e,m)=>(o(),a(u,null,{intro:s((()=>[])),image:s((()=>[d])),heading:s((()=>[t(" Meet the "),p,t(" RGC Team ")])),quote:s((()=>[t(' "Our law firm thrives on a shared workload and collaborative spirit." ')])),"scroll-icon":s((()=>[])),content:s((()=>[r("div",g,[n(i(l))])])),_:1}))}});export{b as default};
--- assets/PublicationsView.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/CmsArticles.js","assets/index.js","assets/index.css","assets/image-url.umd.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{d as e,m as s,w as o,o as a,f as l,a as n,e as t,u as r,L as i,D as u,E as c}from"./index.js";import{_ as m}from"./BodyTemplate.vue_vue_type_script_setup_true_lang.js";import{L as g}from"./LoadingAnimation.js";const p=n("img",{src:i,alt:"Image",class:"object-center shadow-inner lg:min-h-screen lg:object-cover"},null,-1),d=n("br",null,null,-1),_={class:"bg-white bg-cover bg-no-repeat"},b=n("div",{class:"mt-20 flex w-full flex-col items-center justify-between gap-y-10 lg:px-52 xl:flex-row"},null,-1),f=e({__name:"PublicationsView",setup(e){const i=u({loader:()=>c((()=>import("./CmsArticles.js")),__vite__mapDeps([0,1,2,3])),loadingComponent:g});return(e,u)=>(a(),s(m,null,{intro:o((()=>[])),image:o((()=>[p])),heading:o((()=>[l(" RGC's "),d,l(" Knowledge Hub ")])),quote:o((()=>[l(' "Explore our Knowledge Hub for expert insights and valuable resources." ')])),downicon:o((()=>[])),content:o((()=>[n("div",_,[b,t(r(i))])])),_:1}))}});export{f as default};
