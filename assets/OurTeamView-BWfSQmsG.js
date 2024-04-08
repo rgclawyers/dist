a2a110e16d75d4d4e34f9e8a51913e6dd978460b
Ishaan Goel <ishaangoel.99@gmail.com>
2024-04-08 17:42:46 +0530

    update

---
+++ assets/OurTeamView-BWfSQmsG.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/TeamMembers-D71S-Y-O.js","assets/index-D6YgcfyT.js","assets/index-B-zVNMr_.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{d as e,m as a,w as s,o,f as t,a as r,e as n,u as i,y as l,D as m,E as c}from"./index-D6YgcfyT.js";import{_ as u}from"./BodyTemplate.vue_vue_type_script_setup_true_lang-D2j0wEcM.js";import{L as _}from"./LoadingAnimation-BALZwokU.js";const d=r("img",{src:l,alt:"Image",class:"object-center shadow-inner lg:min-h-screen lg:object-cover"},null,-1),g=r("br",null,null,-1),p={id:"bg",class:"bg-white"},h=e({__name:"OurTeamView",setup(e){const l=m({loader:()=>c((()=>import("./TeamMembers-D71S-Y-O.js")),__vite__mapDeps([0,1,2])),loadingComponent:_});return(e,m)=>(o(),a(u,null,{intro:s((()=>[])),image:s((()=>[d])),heading:s((()=>[t(" Meet the "),g,t(" RGC Team ")])),quote:s((()=>[t(' "Our law firm thrives on a shared workload and collaborative spirit." ')])),"scroll-icon":s((()=>[])),content:s((()=>[r("div",p,[n(i(l))])])),_:1}))}});export{h as default};
---
+++ assets/PublicationsView-TMmc3w43.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/CmsArticles-OSnK4x_Q.js","assets/index-D6YgcfyT.js","assets/index-B-zVNMr_.css","assets/image-url.umd-BpRXH6h9.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{d as e,m as s,w as o,o as a,f as l,a as n,e as t,u as r,L as i,D as u,E as c}from"./index-D6YgcfyT.js";import{_ as m}from"./BodyTemplate.vue_vue_type_script_setup_true_lang-D2j0wEcM.js";import{L as g}from"./LoadingAnimation-BALZwokU.js";const p=n("img",{src:i,alt:"Image",class:"object-center shadow-inner lg:min-h-screen lg:object-cover"},null,-1),d=n("br",null,null,-1),_={class:"bg-white bg-cover bg-no-repeat"},b=n("div",{class:"mt-20 flex w-full flex-col items-center justify-between gap-y-10 lg:px-52 xl:flex-row"},null,-1),f=e({__name:"PublicationsView",setup(e){const i=u({loader:()=>c((()=>import("./CmsArticles-OSnK4x_Q.js")),__vite__mapDeps([0,1,2,3])),loadingComponent:g});return(e,u)=>(a(),s(m,null,{intro:o((()=>[])),image:o((()=>[p])),heading:o((()=>[l(" RGC's "),d,l(" Knowledge Hub ")])),quote:o((()=>[l(' "Explore our Knowledge Hub for expert insights and valuable resources." ')])),downicon:o((()=>[])),content:o((()=>[n("div",_,[b,t(r(i))])])),_:1}))}});export{f as default};
