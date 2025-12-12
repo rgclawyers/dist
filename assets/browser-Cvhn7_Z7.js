2d10fc118a2053f2ff5b31f6e9d372a0783dc25d
Ishaan Goel <ishaangoel.99@gmail.com>
2025-12-12 15:59:24 +0530

    add partners

--- assets/browser-Cvhn7_Z7.js
+++ assets/browser-Cvhn7_Z7.js
@@ -1,4 +1,4 @@
-import{H as e}from"./index-BZP1l6g2.js";function t(e,t){for(var n=0;n<t.length;n++){const r=t[n];if("string"!=typeof r&&!Array.isArray(r))for(const t in r)if("default"!==t&&!(t in e)){const n=Object.getOwnPropertyDescriptor(r,t);n&&Object.defineProperty(e,t,n.get?n:{enumerable:!0,get:()=>r[t]})}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}var n,r,o,i={exports:{}},a=i.exports;
+import{H as e}from"./index-BJCnY5V0.js";function t(e,t){for(var n=0;n<t.length;n++){const r=t[n];if("string"!=typeof r&&!Array.isArray(r))for(const t in r)if("default"!==t&&!(t in e)){const n=Object.getOwnPropertyDescriptor(r,t);n&&Object.defineProperty(e,t,n.get?n:{enumerable:!0,get:()=>r[t]})}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}var n,r,o,i={exports:{}},a=i.exports;
 /** @license
  * eventsource.js
  * Available under MIT License (MIT)
